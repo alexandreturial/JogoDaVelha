323e85bb3b70363e4ecfb2be9f70e3314a067908
xande <aleturial8@gmail.com>
2021-04-13 22:06:24 -0300

    components of game

--- lib/Tabuleiro/home.dart
+++ lib/Tabuleiro/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:jogo_da_velha/Components/modal_winner.dart';
 import 'package:jogo_da_velha/Components/poits.dart';
+import 'package:jogo_da_velha/Components/tabuleiro_jogo.dart';
 import 'package:jogo_da_velha/Controller/tabuleiro.dart';
 
 class Home extends StatelessWidget {
-  Tabuleiro tabuleiro = Tabuleiro();
+  final tabuleiro = Tabuleiro();
 
   @override
   Widget build(BuildContext context) {
@@ -12,47 +14,19 @@ class Home extends StatelessWidget {
           children:[ 
           Column(
             children: [
-              Expanded(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.8,
+              Container(
                   color: Colors.white,
-                  padding:EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.2),
-                  child: GridView.count(
-                    crossAxisCount: 3,
-                    padding: EdgeInsets.all(20),
-                    children: List.generate(tabuleiro.casas.length, (index) {
-                      return Center(
-                        child: Observer(builder: (context) {
-                          return TextButton(
-                            style: TextButton.styleFrom(
-                                primary: Colors.teal,
-                                backgroundColor: (tabuleiro.casas[index] == null
-                                    ? Colors.grey[200]
-                                    : tabuleiro.casas[index].typePlayer),
-                                minimumSize: Size(105, 105)),  
-                            onPressed: () {
-                              tabuleiro.nextMove(index);
-                            
-                            },
-                            child: Observer(builder: (context) {
-                              if (tabuleiro.casas[index] == null) {
-                                return Text('');
-                              } else {
-                                return Icon(
-                                  tabuleiro.casas[index].icon,
-                                  color: Colors.white,
-                                  size: 50,
-                                );
-                              }
-                            }),
-                          );
-                        }),
-                      );
-                    }),
-                  ),
-                )
+                  width: MediaQuery.of(context).size.width,
+                  height: 120,
+                  alignment: Alignment.bottomCenter,
+                  child: Text('Jogo da Velha',style: TextStyle(
+                          fontSize: 30, color: 
+                          Colors.black, decoration: 
+                          TextDecoration.none,
+                        ),
+                      )
               ),
+              TabuleiroJogo(tabuleiro),
               Container(
                   color: Colors.white,
                   width: MediaQuery.of(context).size.width,
@@ -72,37 +46,7 @@ class Home extends StatelessWidget {
             
             ]
           ),
-          Observer(builder: (context){
-            if(tabuleiro.isWin){
-              return  Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              color: Colors.black.withOpacity(0.5),
-              
-              child: Align(
-                alignment: Alignment.center,
-                child: TextButton.icon(
-                  label: Text('Novo Jogo'),
-                  icon: Icon(Icons.games),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    padding: EdgeInsets.all(15),
-                    side: BorderSide(color: Colors.white, width: 2),
-                    textStyle: TextStyle(fontSize: 30, ),
-                  ),
-                  onPressed: () {
-                   tabuleiro.newGmae();
-                  }
-                )
-                
-               
-              ),
-            );
-            }else{
-              return SizedBox();
-            }
-          }) 
-         
+          ModalWinner(tabuleiro),
         ]
     );
   }
